ad15bc4e88416f259e36ccc05a053f51001ca322
C250459 <C250459@ccg.ac.jp>
2026-09-03 12:32:15 +0900

    add my name

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       
-
+        <h1>aung myat moe</h1>
       {/* Hero Section */}
       <section className="container py-5">
         <div className="row align-items-center min-vh-75">
